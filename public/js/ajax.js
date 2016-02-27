58de00df6f20c20e3c66f22889f9d858a0e775bb
KevinHu-1024 <hi-bai-du@163.com>
2016-02-27 15:41:50 +0800

    init

---
+++ public/js/ajax.js
@@ -0,0 +1,50 @@
+var btnHome = document.getElementById('btnHome');
+var btnQues = document.getElementById('btnQues');
+var btnAbout = document.getElementById('btnAbout');
+var leftBar = document.getElementById('left-bar');
+var rightContent = document.getElementById('right-content');
+
+
+leftBar.onclick = function (e) {
+	if (e.target.id === "btnHome") {
+		console.log("home");
+		//handle
+		var x1 = new XMLHttpRequest();
+		x1.onreadystatechange=function() {
+		  if (x1.readyState==4 && x1.status==200)
+		    {
+		    rightContent.innerHTML = x1.responseText;
+		    }
+		 }
+		x1.open("GET","main.html",true);
+		x1.send();
+	}
+	if (e.target.id === "btnQues") {
+		console.log("ques");
+		//handle
+		var x2 = new XMLHttpRequest();
+		x2.onreadystatechange=function() {
+		  if (x2.readyState==4 && x2.status==200)
+		    {
+		    rightContent.innerHTML = x2.responseText;
+		    }
+		 }
+		x2.open("GET","ques.html",true);
+		x2.send();
+	}
+	if (e.target.id === "btnAbout") {
+		console.log("about");
+		//handle
+		var x3 = new XMLHttpRequest();
+		x3.onreadystatechange=function() {
+		  if (x3.readyState==4 && x3.status==200)
+		    {
+		    rightContent.innerHTML = x3.responseText;
+		    }
+		 }
+		x3.open("GET","about.html",true);
+		x3.send();
+	}
+}
+
+
