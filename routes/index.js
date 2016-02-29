66da2f7a5b93950ea1a1a84ebde61ecbc4b43503
KevinHu-1024 <hi-bai-du@163.com>
2016-02-29 10:00:47 +0800

    路由分离

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '阿贵龙门客栈' });
-});
-
-module.exports = router;
