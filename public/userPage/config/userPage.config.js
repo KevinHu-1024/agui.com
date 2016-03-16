e9e2f9042e7fb9fe5ce2bacd0faf0c6de01988cf
KevinHu-1024 <hi-bai-du@163.com>
2016-03-16 20:47:22 +0800

    目前进度：admin页面默认显示页面的路由处理。近期找工作，暂停开发。

--- public/userPage/config/userPage.config.js
+++ public/userPage/config/userPage.config.js
@@ -2,19 +2,37 @@ userPage.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('userLog', {
             url: '/userLog',
-            templateUrl: 'userPage/views/userPage.userLog.view.html',
+            templateUrl: 'userPage/views/userPage.userLog.view.html'
         })
         .state('userInfo', {
             url: '/userInfo',
-            templateUrl: 'userPage/views/userPage.userInfo.view.html',
+            templateUrl: 'userPage/views/userPage.userInfo.view.html'
         })
         .state('volunteer', {
             url: '/volunteer',
-            templateUrl: 'userPage/views/userPage.volunteer.view.html',
+            templateUrl: 'userPage/views/userPage.volunteer.view.html'
         })
         .state('admin', {
             url: '/admin',
-            templateUrl: 'userPage/views/userPage.admin.view.html',
+            templateUrl: 'userPage/views/userPage.admin.view.html'
+        })
+        .state('admin.detail', {
+            views: {
+                '': {
+                    templateUrl: 'userPage/views/userPage.admin.main.view.html'
+                },
+                'edit': {
+                    templateUrl: 'userPage/views/userPage.admin.editQuestion.view.html'
+                }
+            }
         });
-    $urlRouterProvider.otherwise('/userLog');        
+        // .state('admin.main', {
+        //     url: '/main',
+        //     templateUrl: 'userPage/views/userPage.admin.main.view.html'
+        // })
+        // .state('admin.edit', {
+        //     url: '/edit',
+        //     templateUrl: 'userPage/views/userPage.admin.editQuestion.view.html'
+        // });
+    $urlRouterProvider.otherwise('/userLog');  
 })
--- routes/routes.js
+++ routes/routes.js
@@ -141,6 +141,7 @@ module.exports = function (app) {
     });
     app.get('/user', function (req, res) {
         res.render('user', {
+                                stylesheets: ['user.css'],
                                 user: req.session.user
                             });
     });
